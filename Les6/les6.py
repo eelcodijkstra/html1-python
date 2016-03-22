580470b209d890d6564d7b1818efd586b986aaa2
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2016-03-22 10:07:02 +0100

    Further cleanup of les 6

--- Les6/les6.py
+++ Les6/les6.py
@@ -16,56 +16,45 @@ render = web.template.render('templates/')
 client = pymongo.MongoClient()
 db = client["tododb"]
 
-def getUser():
-    cookies = web.cookies(username="")
-    username = cookies.username
-    if username != "":
-        # add new user to db, if necessary:
-        user = db.users.find_one({"username": username})
-        if user == None:
-            db.users.insert_one({"username": username})
-        return db.users.find_one({"username": username})
-    else:
-        return None
-
 class Index:
     def GET(self):
-        user = getUser()
-        if user == None:
-            user = {"username": "anonymous", "_id": "0"}
-        return render.index(username=user["username"], userid=user["_id"] )
+        cookies = web.cookies(username="")
+        username = cookies.username
+        if username == "":
+            return render.index(username = "", userid = 0)
+        else:
+            user = db.users.find_one({"username": username})
+            return render.index(username=user["username"], userid=user["_id"])
 
 class Users:
     def GET(self):
         input = web.input()
-        if input.username != "":
+        if input.username == "":
+            return render.index(username="", userid=0)
+        else:
             web.setcookie("username", input.username)
             user = db.users.find_one({"username": input.username})
             if user == None:
-                db.users.insert_one({"username": input.username})
-            user = db.users.find_one({"username": input.username})
-            userid = str(user["_id"])
-            print("user:" + userid)
-            raise web.seeother("/users/" + userid + "/todos")
-        return render.index(username="", userid=0)
+                userid = db.users.insert_one({"username": input.username})
+            else:
+                userid = user["_id"]
+            raise web.seeother("/users/" + str(userid) + "/todos")
 
     def POST(self):
         raise web.seeother("/")
 
 class UserTodoElement:
     def GET(self, userid, eltid):
-        return "GET TodoElement " + "user=" + str(userid) +"elt=" +str(id)
+        return "GET TodoElement " + "user=" + userid + "elt=" + eltid
 
     def POST(self, userid, eltid):
         data = web.input()
-        if not ("done" in data.keys()):
-            data["done"] = False
         db.todos.update_one(
-                {"_id": ObjectId(eltid)},
-                {"$set": {"description": data.descr,
-                           "done": data.done,
-                           "userid": userid}}
-            )
+            {"_id": ObjectId(eltid)},
+            {"$set": {"description": data.descr,
+                      "done": "done" in data.keys(),
+                      "userid": userid}}
+        )
         raise web.seeother("/users/" + userid + "/todos")
 
 class UserTodoList:
@@ -75,12 +64,10 @@ class UserTodoList:
 
     def POST(self, userid):
         data = web.input()
-        if not ("done" in data.keys()):
-            data["done"] = False
         db.todos.insert_one(
-          {"description": data.descr,
-           "done": data.done,
-           "userid": userid}
+            {"description": data.descr,
+             "done": "done" in data.keys(),
+             "userid": userid}
         )
         raise web.seeother("/users/" + userid + "/todos")
 
